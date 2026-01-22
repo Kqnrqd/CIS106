fb12213a10a1ded9051ec3d4d2f8ab6e8e26c73e
Kqnrqd <kk46149@mail.harpercollege.edu>
2026-01-22 12:22:39 -0600

    Update SequenceLogicWeek3.py

--- SequenceLogicWeek3.py
+++ SequenceLogicWeek3.py
@@ -1,4 +1,4 @@
-# 01/20/2026 Name: Konrad Kolber
+# 01/20/2026 Konrad Kolber
 
 print("Problem 1")
 
@@ -60,6 +60,7 @@ circleperimeterresult = 2 * circlepiratio * circleradiusvalue
 
 print("Circle area:", circlearearesult)
 print("Circle perimeter:", circleperimeterresult)
+
 
 
 
